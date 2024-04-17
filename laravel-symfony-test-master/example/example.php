d3448e4df14cbc9b7d3505b48e4683930f54cf3f
Zedsfr <167260856+Zedsfr@users.noreply.github.com>
2024-04-17 18:10:25 +0200

    Add files via upload
    Dans cette première itération, j'ai simplifié les conditions, utilisé des variables temporaires pour stocker les résultats intermédiaires et réorganisé légèrement la logique pour rendre le code plus lisible.

---
+++ laravel-symfony-test-master/example/example.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Scripts;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use App\src\Entity\Template;
+use App\src\Entity\Quote;
+use App\src\Entity\User;
+use App\src\Repository\TemplateManager;
+use Faker\Factory as FakerFactory;
+
+$faker = FakerFactory::create();
+
+$template = new Template(
+    1,
+    'Your delivery to [quote:destination_name]',
+    "
+Hi [user:first_name],
+
+Thanks for contacting us to deliver to [quote:destination_name].
+
+Regards,
+
+SAYNA team
+");
+$templateManager = new TemplateManager();
+
+$message = $templateManager->getTemplateComputed(
+    $template,
+    [
+        'quote' => new Quote($faker->randomNumber(), $faker->randomNumber(), $faker->randomNumber(), $faker->date())
+    ]
+);
+
+echo $message->subject . "\n" . $message->content;
+?>
+
---
+++ laravel-symfony-test-master/src/TemplateManager.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Services;
+
+use App\src\Entity\Template;
+use App\src\Entity\Quote;
+use App\src\Entity\User;
+
+class TemplateManager
+{
+    public function getTemplateComputed(Template $tpl, array $data)
+    {
+        if (!$tpl) {
+            throw new \RuntimeException('no tpl given');
+        }
+
+        $replaced = clone($tpl);
+        $replaced->subject = $this->computeText($replaced->subject, $data);
+        $replaced->content = $this->computeText($replaced->content, $data);
+
+        return $replaced;
+    }
+
+    private function computeText($text, array $data)
+    {
+        $quote = $data['quote'] ?? null;
+
+        if ($quote) {
+            $_quoteFromRepository = QuoteRepository::getInstance()->getById($quote->id);
+            $usefulObject = SiteRepository::getInstance()->getById($quote->siteId);
+            $destinationOfQuote = DestinationRepository::getInstance()->getById($quote->destinationId);
+            $destination = null;
+
+            if(strpos($text, '[quote:destination_link]') !== false) {
+                $destination = DestinationRepository::getInstance()->getById($quote->destinationId);
+            }
+
+            $containsSummaryHtml = strpos($text, '[quote:summary_html]');
+            $containsSummary = strpos($text, '[quote:summary]');
+
+            if ($containsSummaryHtml !== false || $containsSummary !== false) {
+                $summaryHtml = $containsSummaryHtml !== false ? Quote::renderHtml($_quoteFromRepository) : null;
+                $summaryText = $containsSummary !== false ? Quote::renderText($_quoteFromRepository) : null;
+
+                $text = str_replace('[quote:summary_html]', $summaryHtml, $text);
+                $text = str_replace('[quote:summary]', $summaryText, $text);
+            }
+
+            if ($destination) {
+                $destinationLink = $usefulObject->url . '/' . $destination->countryName . '/quote/' . $_quoteFromRepository->id;
+                $text = str_replace('[quote:destination_link]', $destinationLink, $text);
+            } else {
+                $text = str_replace('[quote:destination_link]', '', $text);
+            }
+
+            if (strpos($text, '[quote:destination_name]') !== false) {
+                $text = str_replace('[quote:destination_name]', $destinationOfQuote->countryName, $text);
+            }
+        }
+
+        return $text;
+    }
+}
